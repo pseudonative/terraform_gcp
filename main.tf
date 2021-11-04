8521e168ef3d00a27d7c1bf312f893db11d5fbf0
JeremyDevOps <devopsjeremy@protonmail.com>
2021-11-04 13:39:36 -0600

    third push

--- main.tf
+++ main.tf
@@ -21,3 +21,33 @@ resource "google_project_service" "enabled_service" {
     command = "sleep 15"
   }
 }
+
+resource "google_resourcerepo_repository" "repo" {
+  depends_on = [
+    google_project_service.enabled_service["cloudbuild.googleapis.com"]
+  ]
+  trigger_template {
+    branch_name = "master"
+    repo_name   = google_resourcerepo_repository.repo_name
+  }
+  build {
+    step {
+      name = "gcr.io/cloud-builders/go"
+      args = ["test"]
+      env  = ["PROJECT_ROOT=${var.namespace}"]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", local.image, "."]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", local.image]
+    }
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["run", "deploy", google_cloud_run_service.service.name, "--image", local.image, "--region", var.region, "--platform", "managed", "--q"]
+    }
+  }
+}
+
