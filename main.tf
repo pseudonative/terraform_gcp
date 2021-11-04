a5282bf5ecbfb6d2d411b03e1abecfcdd3dc69b1
JeremyDevOps <devopsjeremy@protonmail.com>
2021-11-04 14:07:09 -0600

    woops

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-locals {
-  services = [
-    "sourcerepo.googleapis.com",
-    "cloudbuild.googleapis.com",
-    "run.googleapis.com",
-    "iam.googleapis.com",
-  ]
-}
-
-resource "google_project_service" "enabled_service" {
-  for_each = toset(local.services)
-  project  = var.project-id
-  service  = each.key
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sleep 60"
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sleep 15"
-  }
-}
-
 resource "google_sourcerepo_repository" "repo" {
   depends_on = [
     google_project_service.enabled_service["sourcerepo.googleapis.com"]
@@ -29,32 +5,25 @@ resource "google_sourcerepo_repository" "repo" {
   name = "${var.namespace}-repo"
 }
 
-resource "google_cloudbuild_trigger" "trigger" {
-  depends_on = [
-    google_project_service.enabled_service["cloudbuild.googleapis.com"]
-  ]
-  trigger_template {
-    branch_name = "master"
-    repo_name   = google_sourcerepo_repository.repo.name
-  }
-  build {
-    step {
+locals {
+  image = "gcr.io/${var.project_id}/${var.namespace}"
+  steps = [
+    {
       name = "gcr.io/cloud-builders/go"
       args = ["test"]
       env  = ["PROJECT_ROOT=${var.namespace}"]
-    }
-    step {
+    },
+    {
       name = "gcr.io/cloud-builders/docker"
       args = ["build", "-t", local.image, "."]
-    }
-    step {
+    },
+    {
       name = "gcr.io/cloud-builders/docker"
       args = ["push", local.image]
-    }
-    step {
+    },
+    {
       name = "gcr.io/cloud-builders/gcloud"
-      args = ["run", "deploy", google_cloud_run_service.service.name, "--image", local.image, "--region", var.region, "--platform", "managed", "--q"]
+      args = ["run", "deploy", google_cloud_run_service.service.name, local.image, "--image", "--region", var.region, "--platform", "managed", "--q"]
     }
-  }
+  ]
 }
-
