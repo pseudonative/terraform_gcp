93e745383d2a3a3a2b08255ee55543a00b66d263
JeremyDevOps <devopsjeremy@protonmail.com>
2021-11-04 13:27:15 -0600

    2nd commit

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ resource "google_project_service" "enabled_service" {
 
   provisioner "local-exec" {
     when    = destroy
+    command = "sleep 60"
+  }
+  provisioner "local-exec" {
+    when    = destroy
     command = "sleep 15"
   }
 }
