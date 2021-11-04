0c10094b45deeeffe3965ab3abb9dd8f2e638465
JeremyDevOps <devopsjeremy@protonmail.com>
2021-11-04 14:30:44 -0600

    final commit

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+locals {
+  services = [
+    "sourcerepo.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "run.googleapis.com",
+    "iam.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "enabled_service" {
+  for_each = toset(local.service)
+  project  = var.project-id
+  service  = each.key
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 15"
+  }
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
 resource "google_sourcerepo_repository" "repo" {
   depends_on = [
     google_project_service.enabled_service["sourcerepo.googleapis.com"]
@@ -27,3 +48,66 @@ locals {
     }
   ]
 }
+
+resource "google_cloudbuild_trigger" "trigger" {
+  depends_on = [
+    google_project_service.enabled_service["cloudbuild.googleapis.com"]
+  ]
+  trigger_template {
+    branch_name = "master"
+    repo_name   = google_sourcerepo_repository.repo.name
+  }
+  build {
+    dynamic "step" {
+      for_each = local.steps
+      content {
+        name = step.value.name
+        args = step.value.args
+        env  = lookup(step.value, "env", null)
+      }
+    }
+  }
+}
+
+data "google_project" "project" {}
+
+resource "google_project_iam_member" "cloudbuild_roles" {
+  depends_on = [
+    google_cloudbuild_trigger.trigger
+  ]
+  for_each = toset(["roles/run.admin", "roles/iam.serviceAccountUser"])
+  project  = var.project-id
+  role     = each.key
+  member   = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_cloud_run_service" "service" {
+  depends_on = [
+    google_project_service.enabled_service["run.googleapis.com"]
+  ]
+  name     = var.namespace
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers", ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "policy" {
+  location    = var.region
+  project     = var.project-id
+  service     = google_cloud_run_service.service.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
